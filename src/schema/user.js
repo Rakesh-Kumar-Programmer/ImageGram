a4c7eeb9d16413c3c2e61afda212db690f94fd86
Rakesh Kumar <rakesh.bahel@gmail.com>
2024-10-29 23:02:41 +0530

    create schema

---
+++ src/schema/user.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userschema = new mongoose.Schema({
+    username : {
+        type : String,
+        required : true,
+        unique : true,
+        minlength : 3,
+        maxlength : 16
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true,
+        match : /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        validate: {
+            validator: function(v) {
+                return /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(v);
+            },
+            message: "Invalid email format"
+        },
+        lowercase : true,
+        maxlength : 50,
+        minlength : 5
+    }
+})    
